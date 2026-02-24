c5483b847a2621dc684c669abacd6d25d46d8779
YUDINDIN1005 <109646304+YUDINDIN1005@users.noreply.github.com>
2026-02-24 22:24:14 +0900

    mod: stt 모듈 공개 엔진 목록 수정

--- src/ansimon_ai/stt/__init__.py
+++ src/ansimon_ai/stt/__init__.py
@@ -1,10 +1,12 @@
 from .types import STTResult, STTSegment
 from .base import STTEngine
 from .mock import MockSTT
+from .whisper_stt import WhisperSTT
 
 __all__ = [
     "STTResult",
     "STTSegment",
     "STTEngine",
     "MockSTT",
+    "WhisperSTT",
 ]
